6a82b2d35628eaa64d655ecd1682bf2d65c1cbd1
AlejoIbarra <andresibarra@umariana.edu.co>
2026-09-13 23:05:12 -0500

    feat: add local technology logo assets and update SwiperComponent to use them

--- components/SwiperComponent.vue
+++ components/SwiperComponent.vue
@@ -45,39 +45,27 @@ const breakpoints = {
 
 const logos = [
   {
-    src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Spring_Framework_Logo_2018.svg/1200px-Spring_Framework_Logo_2018.svg.png',
-    alt: 'Logo Spring Framework',
+    src: '/img/technologies/spring.png',
+    alt: 'Logo Spring Boot',
   },
   {
-    src: 'https://logos-world.net/wp-content/uploads/2022/07/Java-Symbol.png',
-    alt: 'Logo Java',
-  },
-  {
-    src: 'https://www.cdnlogo.com/logos/a/35/angular.svg',
+    src: '/img/technologies/Angular.png',
     alt: 'Logo Angular Framework',
   },
   {
-    src: 'https://www.southeastlinuxfest.org/wp-content/uploads/2016/04/postgresql-logo.png',
-    alt: 'Logo Postgres Database',
+    src: '/img/technologies/mongo.png',
+    alt: 'Logo MongoDB',
   },
   {
-    src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/MongoDB_Logo.svg/1200px-MongoDB_Logo.svg.png',
-    alt: 'Logo Mongo DB',
-  },
-  {
-    src: 'https://cdn.icon-icons.com/icons2/2699/PNG/512/laravel_logo_icon_170314.png',
-    alt: 'Logo Laravel Framework',
-  },
-  {
-    src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/PHP-logo.svg/2560px-PHP-logo.svg.png',
+    src: '/img/technologies/php.png',
     alt: 'Logo PHP',
   },
   {
-    src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9d/Swift_logo.svg/1200px-Swift_logo.svg.png',
+    src: '/img/technologies/swift.png',
     alt: 'Logo Swift Apple',
   },
   {
-    src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Google-flutter-logo.svg/2560px-Google-flutter-logo.svg.png',
+    src: '/img/technologies/flutter.png',
     alt: 'Logo Flutter Google',
   },
 ]
